d70b0cfdb18254290b6f25fceae9cb543b44e15a
alexn400 <alexn400@gmail.com>
2022-03-03 17:49:55 +1300

    Setup MUI

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -3,10 +3,15 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { CssBaseline, ThemeProvider } from '@mui/material';
+import theme from './theme';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+        <App />
+      </ThemeProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
