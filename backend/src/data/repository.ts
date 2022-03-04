1014edfbcb5f00aac8bed3f013e6033219227b5e
Peter Goedeke <peterbgoedeke@gmail.com>
2022-03-04 17:29:49 +1300

    feat: repository service with required database calls for endpoints
    This commit also changes the User model's id field to a string in
    the api spec, as the id will be generated by Firebase as a UUID

--- backend/src/data/repository.ts
+++ backend/src/data/repository.ts
@@ -1,6 +1,115 @@
 import prisma from './db'
-import {} from '@prisma/client'
+import { Prisma, Post, User, Category } from '@prisma/client'
 
-const repository = {}
+const repository = {
+    async createPost(userId: string, post: Components.Schemas.CreatePost) {
+        return await prisma.post.create({
+            data: {
+                content: post.content,
+                timestampCreated: Date.now(),
+                timestampModified: Date.now(),
+                categories: post.categories as string[],
+                resource: post.resource || null,
+                userId
+            }
+        })
+    },
+
+    async likePost(userId: string, postId: number) {
+        return await prisma.post.update({
+            where: {
+                id: postId
+            },
+            data: {
+                likedUsers: {
+                    connect: {
+                        id: userId
+                    }
+                }
+            }
+        })
+    },
+
+    async unlikePost(userId: string, postId: number) {
+        return await prisma.post.update({
+            where: {
+                id: postId
+            },
+            data: {
+                likedUsers: {
+                    delete: {
+                        id: userId
+                    }
+                }
+            }
+        })
+    },
+
+    async getPostById(id: number) {
+        return await prisma.post.findUnique({
+            where: {
+                id
+            }
+        })
+    },
+
+    async updatePost(postId: number, post: Components.Schemas.CreatePost) {
+        return await prisma.post.update({
+            where: {
+                id: postId
+            },
+            data: {
+                content: post.content,
+                timestampModified: Date.now(),
+                categories: post.categories as string[],
+                resource: post.resource || null
+            }
+        })
+    },
+
+    async getPosts(query: Components.Schemas.GetPostQuery) {
+        return await prisma.post.findMany({
+            where: {
+                userId: query.userIdQuery,
+                content: {
+                    contains: query.searchQuery
+                },
+                categories: {
+                    hasEvery: query.searchCategories
+                }
+            },
+            include: {
+                user: true,
+                likedUsers: true
+            },
+            take: query.pageSize,
+            skip: query.pageSize && query.offset ? query.pageSize * query.offset : undefined
+        })
+    },
+
+    async getCategories() {
+        return await prisma.category.findMany()
+    },
+
+    async createCategory(name: string) {
+        return await prisma.category.create({
+            data: {
+                name
+            }
+        })
+    },
+
+    async getUserById(userId: string) {
+        return await prisma.user.findUnique({
+            where: {
+                id: userId
+            },
+            include: {
+                followed: true,
+                followers: true
+            }
+        })
+    }
+}
 
 export default repository
