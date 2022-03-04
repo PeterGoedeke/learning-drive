92fb65c1c0953c4432c21588e6c949c650d484b4
Peter Goedeke <peterbgoedeke@gmail.com>
2022-03-04 16:07:41 +1300

    feat: boilerplate types for controllers

---
+++ backend/src/controllers/categories.ts
@@ -0,0 +1,12 @@
+import { Request, Response } from 'express'
+import { StatusCodes } from 'http-status-codes'
+import asyncHandler from 'express-async-handler'
+import Logger from '../logger'
+import { ParamsDictionary } from 'express-serve-static-core/index'
+
+type getCategories = (req: Request, res: Response<Paths.GetCategories.Responses.$200>) => void
+
+type createCategory = (
+    req: Request<ParamsDictionary, {}, Paths.CreateCategory.RequestBody>,
+    res: Response<Paths.CreateCategory.Responses.$201 | Paths.CreateCategory.Responses.$400>
+) => void
---
+++ backend/src/controllers/posts-id.ts
@@ -0,0 +1,28 @@
+import { Request, Response } from 'express'
+import { StatusCodes } from 'http-status-codes'
+import asyncHandler from 'express-async-handler'
+import Logger from '../logger'
+import { ParamsDictionary } from 'express-serve-static-core/index'
+
+type getPostById = (
+    req: Request,
+    res: Response<Paths.GetPostById.Responses.$200 | Paths.GetPostById.Responses.$404>
+) => void
+
+type updatePost = (
+    req: Request<ParamsDictionary, {}, Paths.UpdatePost.RequestBody>,
+    res: Response<
+        | Paths.UpdatePost.Responses.$204
+        | Paths.UpdatePost.Responses.$400
+        | Paths.UpdatePost.Responses.$404
+    >
+) => void
+
+type reactToPost = (
+    req: Request<ParamsDictionary, {}, Paths.ReactToPost.RequestBody>,
+    res: Response<
+        | Paths.ReactToPost.Responses.$204
+        | Paths.ReactToPost.Responses.$400
+        | Paths.ReactToPost.Responses.$404
+    >
+) => void
