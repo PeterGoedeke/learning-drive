78ec0b89bf2f31097f8551eaf04595eccc7c9e34
Kinan Cheung <kinancheung@gmail.com>
2022-03-04 20:49:31 +1300

    Squashed commit of the following:
    commit d4e98ec9490635e09beac400d2876ce3ac8fcf50
    Author: alexn400 <alexn400@gmail.com>
    Date:   Fri Mar 4 19:15:33 2022 +1300
    
        Add stories for page header
    
    commit 6bb6faae0878c0038dddbfdc34f682a235d5fbd1
    Author: alexn400 <alexn400@gmail.com>
    Date:   Fri Mar 4 19:15:24 2022 +1300
    
        Create page header component
    
    commit 9cc4785d418cc3b33b86c8aa88db5fd56e3d61a1
    Author: alexn400 <alexn400@gmail.com>
    Date:   Fri Mar 4 17:25:47 2022 +1300
    
        Add icons with storybook
    
    commit b3016ba732348d6829aecf8494a37f0f591c8773
    Author: alexn400 <alexn400@gmail.com>
    Date:   Fri Mar 4 16:05:58 2022 +1300
    
        Export theme from barrel file
    
    commit e2703172b223bff6eafe73466b99e8f95a2c557b
    Author: alexn400 <alexn400@gmail.com>
    Date:   Fri Mar 4 15:59:55 2022 +1300
    
        Setup theme

---
+++ frontend/src/components/layout/PageHeader/PageHeader.stories.tsx
@@ -0,0 +1,37 @@
+import { IconButton } from '@mui/material';
+import { Meta, Story } from '@storybook/react';
+
+import PageHeader, { PageHeaderProps } from '.';
+
+import SearchIcon from '../../icons/SearchIcon';
+
+export default {
+  component: PageHeader,
+  title: 'Components/PageHeader',
+  parameters: { layout: 'fullscreen' },
+} as Meta;
+
+interface PageHeaderStoryProps extends PageHeaderProps {}
+
+const Template: Story<PageHeaderStoryProps> = (args) => <PageHeader {...args} />;
+
+export const Default = Template.bind({});
+Default.args = {
+  title: 'Global Feed',
+};
+
+export const WithAction = Template.bind({});
+WithAction.args = {
+  title: 'Global Feed',
+  action: (
+    <IconButton color='secondary'>
+      <SearchIcon />
+    </IconButton>
+  ),
+};
+
+export const WithBackButton = Template.bind({});
+WithBackButton.args = {
+  title: 'Global Feed',
+  backButton: true,
+};
