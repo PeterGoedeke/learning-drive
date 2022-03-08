3d345b74292f7f2cfb1edbd7a908046aa2474482
alexn400 <alexn400@gmail.com>
2022-03-08 15:39:31 +1300

    Add cors

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -7,8 +7,11 @@ import * as OpenApiValidator from 'express-openapi-validator'
 import { ValidationError } from 'express-openapi-validator/dist/framework/types'
 import Logger from './util/logger'
 import expressWinston from 'express-winston'
+import cors from 'cors'
 
 const app = express()
+
+app.use(cors())
 
 app.use(express.json())
 app.use(express.text())
